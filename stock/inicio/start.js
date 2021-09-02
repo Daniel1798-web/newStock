7f65dde7782f6e4c7e1479e7ee6f5096739ef4c5
Daniel1798-web <64641943+Daniel1798-web@users.noreply.github.com>
2021-09-01 23:48:20 -0300

    p

--- stock/inicio/start.js
+++ stock/inicio/start.js
@@ -5,11 +5,18 @@ var botonInicio = document.getElementById("inicio");
 var escondido = document.getElementById("escondido")
 
 botonInicio.addEventListener("mouseover" , showElements)
+botonInicio.addEventListener("mouseout", closeElements)
 
 function showElements()
 {
 
  escondido.className = "revelado";
- botonInicio.removeEventListener("mouseover", showElements)
+
      console.log("hello")
+}
+
+function closeElements()
+{
+    escondido.className = "hiden";
+    console.log("bye")
 }
