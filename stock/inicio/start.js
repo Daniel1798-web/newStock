b5c492238c6de1dffd3051dcbd71bbe8136db0d1
Daniel1798-web <64641943+Daniel1798-web@users.noreply.github.com>
2021-10-08 23:16:38 -0300

    l

--- stock/inicio/start.js
+++ stock/inicio/start.js
@@ -3,20 +3,22 @@
 var divPapa = document.getElementById("divPapa");
 var botonInicio = document.getElementById("inicio");
 var escondido = document.getElementById("escondido")
+var imagen2 = document.getElementById("imagen2")
+var imagen3 = document.getElementById("imagen3")
+var infoMessage  = document.getElementById("infoMessage");
+imagen2.addEventListener("click", enlace)
+imagen3.addEventListener("click", info)
 
-botonInicio.addEventListener("mouseover" , showElements)
-botonInicio.addEventListener("mouseout", closeElements)
-
-function showElements()
+function enlace()
 {
-
- escondido.className = "revelado";
-
-     console.log("hello")
+    window.open("http://127.0.0.1:5500/stock/inicio/stock.html", "_blanck")
 }
 
-function closeElements()
+function info()
 {
-    escondido.className = "hiden";
-    console.log("bye")
+    
+    let textInfo = document.createTextNode( "Hola¡  si tiene una duda comuniquese con nuestro servicio Tecnico");    
+    infoMessage.appendChild(textInfo)
+    console.log("hols")
+
 }
