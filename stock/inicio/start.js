7d0181f0862a1d6b55a7cc9944797b22bce79415
Daniel1798-web <64641943+Daniel1798-web@users.noreply.github.com>
2021-10-19 23:06:46 -0300

    a loot of changes

--- stock/inicio/start.js
+++ stock/inicio/start.js
@@ -3,22 +3,25 @@
 var divPapa = document.getElementById("divPapa");
 var botonInicio = document.getElementById("inicio");
 var escondido = document.getElementById("escondido")
+var imagen1 = document.getElementById("imagen1")
 var imagen2 = document.getElementById("imagen2")
 var imagen3 = document.getElementById("imagen3")
-var infoMessage  = document.getElementById("infoMessage");
-imagen2.addEventListener("click", enlace)
+
+imagen1.addEventListener("click", enlace)
+imagen2.addEventListener("click", enlace2)
 imagen3.addEventListener("click", info)
 
 function enlace()
 {
-    window.open("http://127.0.0.1:5500/stock/inicio/stock.html", "_blanck")
+    window.open("http://127.0.0.1:5501/stock/inicio/stock.html","_blanck")
 }
 
 function info()
 {
-    
-    let textInfo = document.createTextNode( "Hola¡  si tiene una duda comuniquese con nuestro servicio Tecnico");    
-    infoMessage.appendChild(textInfo)
-    console.log("hols")
+    alert("Hola¡, estas en problemas? comunicate al /*/*/*/*/*")
+}
 
+function enlace2()
+{
+    window.open("")
 }
